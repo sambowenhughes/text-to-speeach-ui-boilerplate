13999d4ad4b7c96e7dd67639a33f3076d3a9f7ce
Sam <sambowenhughes@hotmail.co.uk>
2023-08-22 11:49:52 +0800

    Created Boiler Plate

---
+++ app/views/GenerateSoundView.tsx
@@ -0,0 +1,69 @@
+'use client' 
+
+import { GenerateSoundForm } from "@/components/GenerateSoundForm";
+import Loader from "@/components/Loader";
+import { useState } from "react";
+
+/**
+ * Represents the request payload for generating sound using a pre-trained model.
+ */
+export interface CreateSoundRequest {
+    /**
+     * The URL of the pre-trained model to be used for sound generation.
+     */
+    modelUrl: string;
+  
+    /**
+     * The input text that will be used to generate the sound.
+     */
+    text: string;
+  }
+  
+
+/**
+ * The main view component for generating sound using a pre-trained model.
+ */
+export default function GenerateSoundView() {
+  // State to manage loading status and audio URL
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
+
+  /**
+   * Handles the process of fetching audio data using the provided request.
+   * @param {CreateSoundRequest} request - The request containing model URL and text.
+   */
+  const handleGetAudio = async (request: CreateSoundRequest) => {
+    setIsLoading(true);
+
+    alert("Hello")
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="w-full md:w-1/3 p-4">
+        <div className="ml-8 mr-8 mt-4 mb-4 text-xl">
+          <h1>Text to Speech</h1>
+        </div>
+        {/* Render the form component for generating sound */}
+        <GenerateSoundForm handleGetAudio={handleGetAudio} />
+      </div>
+      <div className="w-full md:w-2/3 p-4 bg-gray-200 h-screen">
+        <div className="h-full flex justify-center items-center">
+          {isLoading ? (
+            // Show loader when fetching audio data
+            <Loader />
+          ) : (
+            // Display audio player when audio is available
+            <>
+              {audioUrl && (
+                <audio controls>
+                  <source id="audioSource" type="audio/flac" src={audioUrl!} />
+                </audio>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/GenerateSoundForm.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+// Import necessary modules and components
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+import { Textarea } from "./ui/textarea";
+import SOUND_MODELS, { SoundModel } from "@/lib/constants";
+import { CreateSoundRequest } from "@/app/views/GenerateSoundView";
+
+// Define the validation schema for the form fields
+const FormSchema = z.object({
+  soundModel: z.string({
+    required_error: "Please select a Hugging Face sound model to use.",
+  }),
+  text: z.string({
+    required_error: "Please select a text for the model to use.",
+  }),
+});
+
+// Define the props interface for the GenerateSoundForm component
+interface GenerateSoundFormProps {
+  handleGetAudio: (data: CreateSoundRequest) => void;
+}
+
+// Main component function
+export function GenerateSoundForm({ handleGetAudio }: GenerateSoundFormProps) {
+  // State for tracking form submission status
+  const [formSubmitting, setFormSubmitting] = useState<boolean>(false);
+
+  // Initialize the react-hook-form with the validation schema
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  // Function to handle form submission
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    setFormSubmitting(true);
+    
+    // Prepare the sound request object
+    const soundRequest: CreateSoundRequest = {
+      modelUrl: data.soundModel,
+      text: data.text,
+    };
+    
+    // Call the provided handler function with the sound request
+    handleGetAudio(soundRequest);
+    
+    setFormSubmitting(false);
+  }
+
+  return (
+    <div className="ml-8 mr-8">
+      {/* Form component that uses react-hook-form */}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          {/* Form field for selecting the sound model */}
+          <FormField
+            control={form.control}
+            name="soundModel"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Sound Model</FormLabel>
+                {/* Select component for choosing a sound model */}
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  disabled={formSubmitting}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a verified email to display" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {/* Map through available sound models */}
+                    {SOUND_MODELS.map((model: SoundModel, index: number) => (
+                      <SelectItem key={`${model.name}-${index}`} value={model.url}>
+                        {model.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormDescription>
+                  This model will generate your sound.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Form field for entering the text */}
+          <FormField
+            control={form.control}
+            name="text"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Text</FormLabel>
+                <FormControl>
+                  {/* Textarea component for entering text */}
+                  <Textarea
+                    disabled={formSubmitting}
+                    rows={6}
+                    placeholder="Enter your text here..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  The text used to convert to sound.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Submit button */}
+          <Button type="submit" disabled={formSubmitting}>
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
